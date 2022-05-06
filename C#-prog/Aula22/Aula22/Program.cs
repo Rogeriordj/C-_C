b5ca4c8b1b29218142ea0ee3c707aff03d666139
Rogerio <rogeriordj@gmail.com>
2022-05-06 08:32:30 -0300

    exerc

---
+++ C#-prog/Aula22/Aula22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x;
+            int y;
+            double z;
+            char w;
+
+            Console.WriteLine("Informe o X - string: ");
+            x = Console.ReadLine();
+
+            Console.WriteLine("Informe o Y inteiro: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o z double: ");
+            z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe o w: ");
+            w = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(w);
+
+            Console.ReadLine();
+        }
+    }
+}
